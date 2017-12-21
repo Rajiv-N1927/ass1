3ba77afac223b39df26d6857a1b50dd6f8c7fdf8
Rajiv Narayan <Rajiv.narayan12@gmail.com>
2017-12-21 11:46:21 +1100

    intro.cpp

---
+++ intro.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <iterator>
+#include <cmath>
+#include <string>
+
+void interface(std::vector<std::string> test);
+void handle(std::vector<std::string>::iterator st,
+            std::vector<std::string>::iterator ptr, int &ans);
+int checkcmd(std::string str);
+
+int main(int argc, char* argv[]) {
+
+    // setup the print out format for the precision required.
+    std::cout.setf(std::ios::fixed,std::ios::floatfield);
+    std::cout.precision(3);
+
+    // open the file for reading
+    std::ifstream in;
+    in.open(argv[1]);
+
+    // string to be read into
+    std::string s;
+
+    //Vector
+    std::vector<std::string> first;
+
+    // read the file while we have input.
+    while (in >> s) {
+      first.push_back(s);
+    }
+    interface(first);
+    in.close();
+}
+void interface(std::vector<std::string> test) {
+  std::vector<std::string>::iterator start, ptr, end;
+  //std::vector<std::string> array;
+  ptr = start = test.begin();
+  end = test.end();
+  int eos = 1;
+  int ans;
+  while ( eos ) {
+    if ( checkcmd(*ptr) ) {
+      handle(start, ptr, ans);
+      std::string anss = std::to_string(ans);
+      test.erase(start, ptr+1);
+      ptr = test.begin();
+      end = test.end();
+      test.insert(start, anss);
+      start = ptr+1;
+    }
+
+    if ( ptr == end ) eos = 0;
+    *ptr++;
+  }
+
+}
+
+void handle(std::vector<std::string>::iterator st,
+            std::vector<std::string>::iterator ptr, int &ans) {
+
+  int num1, num2;
+
+  if ( (ptr - 1) >= st) num1 = stoi(*(ptr - 1));
+  if ( (ptr - 2) >= st) num2 = stoi(*(ptr - 2));
+  std::string toswitch = *ptr;
+
+  if ( toswitch == "add" ) ans = num1 + num2;
+  else if ( toswitch == "sub" ) ans = num1 - num2;
+  else if ( toswitch == "div" ) ans = num1 / num2;
+  else if ( toswitch == "mult" ) ans = num1 * num2;
+  else ans = 0;
+
+}
+
+int checkcmd(std::string str) {
+  std::string::iterator it;
+  it = str.begin();
+  int retval = 0;
+  if ( (*it >= 65 && *it <= 90)
+      || (*it >= 97 && *it <= 122) )
+  { retval = 1; }
+  return retval;
+}
